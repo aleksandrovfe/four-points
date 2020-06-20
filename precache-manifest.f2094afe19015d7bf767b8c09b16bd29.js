fbc1659ee3c462f7b367d2768cd24cf3097c2fe8
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 08:08:46 +0300

    Updates

---
+++ precache-manifest.f2094afe19015d7bf767b8c09b16bd29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2728f7f9b117e8c5fd6fa839e561c6",
+    "url": "/four-points/index.html"
+  },
+  {
+    "revision": "880e47fecb563130856c",
+    "url": "/four-points/static/css/main.208db870.chunk.css"
+  },
+  {
+    "revision": "ba653f549180d49b05ef",
+    "url": "/four-points/static/js/2.45ccdb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/four-points/static/js/2.45ccdb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880e47fecb563130856c",
+    "url": "/four-points/static/js/main.fe6eb0dd.chunk.js"
+  },
+  {
+    "revision": "9a40df26b1848aae23d6",
+    "url": "/four-points/static/js/runtime-main.f789d945.js"
+  }
+]);
